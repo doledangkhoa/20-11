39281824aa2c6a370a72343f2cd53f9daef12521
gasieuwaycuatk20 <67224126+gasieuwaycuatk20@users.noreply.github.com>
2021-11-15 15:12:59 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "20/11",
+    introTitle: 'Gửi anh Sơn',
+    introDesc: `Đã 3 năm kể từ ngày anh làm cố vấn học tập cho chúng em. Anh đã giúp đỡ chúng em rất nhiều và rất nhiệt tình. Vì thế nhân ngày 20/11 chúng em quyết định làm tặng anh 1 món quà nho nhỏ. Mời anh bấm oke để nhận được 1 món quà trên cả tuyết vời từ tụi em 😂`,
+    btnIntro: '^^Oke^^',
+    title: 'Món qùa có giá trị rất cao nên anh hãy giữ bình tĩnh để tiếp tục',
+    desc: 'Anh có muốn tiếp tục không ???',
+    btnYes: 'Để ngắm tấm hình dìm deeptry thêm xíu đã !!!',
+    btnNo: 'Tiếp luôn cho máu =)))',
+    question: 'Xin cho biết tâm trạng của anh lúc này',
+    btnReply: 'Gửi <3',
+    reply: 'Tất nhiên là vô cùng vui rồi =)))))',
+    mess: 'Happy Teacher Day - 20/11',
+    messDesc: 'Chúng em chúc anh ngày càng có nhiều sức khỏe, thành công và niềm vui trong công việc, cuộc sống',
+    btnAccept: 'Cám ơn mấy đứa <3',
+    messLink: 'https://github.com/gasieuwaycuatk20/20-11'
+}
